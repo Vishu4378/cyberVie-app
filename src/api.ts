bd5705b77227ca875ca33929d236b4699d26f484
Vijay Pandey <98205038+Vishu4378@users.noreply.github.com>
2022-12-06 14:57:50 +0530

    Update api.ts
    "api added"

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const uploadDocs = (file: any) => {
-  return axios.post("http://localhost:5000/upload", file).then((res) => {
+  return axios.post("https://cybervie.onrender.com/upload", file).then((res) => {
     return res;
   });
 };
 
 export const viewUser = () => {
-  return axios.get("http://localhost:5000/view").then((res) => {
+  return axios.get("https://cybervie.onrender.com/view").then((res) => {
     return res.data;
   });
 };
